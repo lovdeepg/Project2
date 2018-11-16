819d37e2bc089bf4c65da102cb637e250315b4eb
Lovdeep <lovdeepgahunia1@gmail.com>
2018-11-16 12:12:07 -0500

    finished crud

--- controllers/videoplaylistcontroller.js
+++ controllers/videoplaylistcontroller.js
@@ -14,10 +14,6 @@ create videoplaylist inside the post action
 //   res.send("this is the test  route");
 // });
 
-// router.get("/", (req, res) => {
-//   res.redirect("index");
-// });
-
 router.get("/", (req, res) => {
   VideoPlaylist.find({}).then(videos => {
     res.render("index", { videos });
@@ -30,30 +26,144 @@ router.get("/new", (req, res) => {
 });
 
 // this is for create
-router.post("/new", (req, res) => {
+router.post("/", (req, res) => {
   VideoPlaylist.create({
     title: req.body.title,
     url: req.body.url,
-    comments: req.body.comments
-  })
-    .then(videos => {
-      res.redirect("new" + { videos });
-    })
-    .catch(err => {
-      console.log(err);
-    });
-});
-// this is for show
-router.get("/:id", (req, res) => {
-  VideoPlaylist.findOne({ _id: req.params.id }).then(videos => {
-    res.redirect("show", { videos });
+    comments: req.body.content
+  }).then(videos => {
+    res.redirect("/");
   });
 });
+// this is for show
+// router.get("/:id", (req, res) => {
+//   VideoPlaylist.findOne({ _id: req.params.id }).then(videos => {
+//     res.render("show", videos);
+//   });
+// });
 
-router.get("edit/:id", (req, res) => {
+// this should be get route for update
+router.get("/edit/:id", (req, res) => {
   VideoPlaylist.findById(req.params.id).then(video => {
     res.render("edit", video);
   });
 });
 
+// this should be for update
+router.put("/update/:id", (req, res) => {
+  VideoPlaylist.findOneAndUpdate({ _id: req.params.id }, req.body, {
+    new: true
+  }).then(video => {
+    console.log(video);
+    res.redirect("/");
+  });
+});
+
+router.get("/videoplaylist/delete/:id", (req, res) => {
+  VideoPlaylist.findOneAndRemove({ _id: req.params.id }).then(video => {
+    console.log(video);
+    res.redirect("/");
+  });
+});
+
+// router.get("/", (req, res) => {
+//   res.redirect("/index");
+// });
 module.exports = router;
+
+// not sure need to ask about this code
+
+//   res.render("videoplalist/index");
+// });
+
+// this is for show
+// router.get("/videoplaylist/:id", (req, res) => {
+//   VideoPlaylist.findOne({ _id: req.params.id }).then(video => {
+//     res.render("videoplaylist/show", video);
+//   });
+// });
+// in the videoplaylist route
+// router.get("/videoplaylist", (req, res) => {
+//   VideoPlaylist.find().then(videos => {
+//     res.render("videoplaylist/index", videos);
+//   });
+// });
+
+// to get new form
+// router.get("/videoplaylist/new", (req, res) => {
+//   res.render("videoplaylist/new");
+// });
+
+// this is for create
+// router.post("/videoplaylist", (req, res) => {
+//   VideoPlaylist.create({
+//     title: req.body.title,
+//     url: req.body.url,
+//     comments: req.body.comments
+//   }).then(video => {
+//     res.redirect("/videoplaylist/" + video.id);
+//   });
+// });
+// this should be for edit
+// router.get("/videoplaylist/edit/:id", (req, res) => {
+//   VideoPlaylist.findById(req.params.id).then(video => {
+//     res.render("videoplaylist/edit", video);
+//   });
+// });
+
+// this is for updating
+// router.put("/videoplaylist/:id", (req, res) => {
+//   VideoPlaylist.findOneAndUpdate({ _id: req.params.id }, req.body, {
+//     new: true
+//   }).then(video => {
+//     video.comments.push(comments);
+//   });
+//   video.save(err => {
+//     res.redirect("/videoplaylist");
+//   });
+// });
+
+// const mongoose = require("../db/connection");
+
+// const VideoPlaylist = mongoose.model("Videoplaylist");
+
+// router.get("/", (req, res) => {
+//   VideoPlaylist.find({}).then(videos => res.json(videos));
+// });
+
+// router.get("/:id", (req, res) => {
+//   VideoPlaylist.findById(req.params.id).then(video => {
+//     res.json(video);
+//   });
+// });
+
+// router.post("/", (req, res) => {
+//   VideoPlaylist.create({
+//     title: req.body.title,
+//     url: req.body.url,
+//     comments: req.content
+//   }).then(video => {
+//     req.comments.push({
+//       content
+//     });
+//     // req.comments.save(err => {
+//     //   res.redirect(`/`)
+//     // });
+//     res.json(video);
+//   });
+// });
+
+// router.put("/:id", (req, res) => {
+//   VideoPlaylist.findOneAndUpdate({ _id: req.params.id }, req.body).then(video =>
+//     VideoPlaylist.find({}).then(videos => {
+//       res.json(videos);
+//     })
+//   );
+// });
+
+// router.delete("/:id", (req, res) => {
+//   VideoPlaylist.findOneAndRemove({ _id: req.params.id }, req.body).then(video =>
+//     VideoPlaylist.find({}).then(videos => {
+//       res.json(videos);
+//   })
+// );
--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const hbs = require("hbs");
 const parser = require("body-parser");
+const methodoverride = require("method-override");
 const playlistController = require("./controllers/videoplaylistcontroller");
+require("pretty-error").start();
 
 // const cors = require("cors");
 const app = express();
 
 app.set("view engine", "hbs");
 // app.use(cors());
-
+app.use(methodoverride("_method"));
 app.use(parser.json()); // interprets a stringified JSON object on the request body
 app.use(parser.urlencoded({ extended: true })); // interprets key value pairs in URLs
 app.use(express.static(__dirname + "/public"));
